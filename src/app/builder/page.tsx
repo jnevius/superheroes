9663e483b6bb692151c1368012114f189db414d9
Joel Nevius <120746669+jnevius@users.noreply.github.com>
2024-07-20 22:30:39 -0700

    adding data for characters

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -3,10 +3,15 @@ import marvel from '@/../../public/Marvel.png'
 import Image from 'next/image';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { H1, H2 } from '@/components/ui/typography';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-// import { heroes } from '@/data/heroData';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import heroes from '@/app/data/heroes.json'
 import { Input } from '@/components/ui/input';
-import thor from '@/../../public/cards/thor.jpg'
+import thorpic from '@/../../public/cards/thor.jpg'
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+
+const heroNames = heroes.map(hero => hero.name);
+const thor = heroes[0];
 
 
 export default function Builder() {
@@ -19,9 +24,9 @@ export default function Builder() {
                         alt="Marvel"
                         height={40}
                     />
-                    <H1 className='text-3xl'>SUPERPOWERED</H1>
+                    <H2 className='font-semibold'>SUPERPOWERED!</H2>
                 </div>
-                <Avatar className='hover:ring-2 hover:ring-red-400 cursor-pointer'>
+                <Avatar className='hover:ring-2 hover:ring-orange-500 cursor-pointer'>
                     <AvatarImage className='object-cover' src='https://images.unsplash.com/photo-1463453091185-61582044d556?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
                     <AvatarFallback>TH</AvatarFallback>
                 </Avatar>
@@ -30,70 +35,94 @@ export default function Builder() {
             <section className='flex flex-col bg-slate-50 h-[1000px] w-full p-8 gap-10'>
                 <div className='flex flex-col gap-5'>
                     <div>
-                        <H1>Your Team</H1>
+                        <H1 className='text-4xl'>My Powers</H1>
                     </div>
-                    <div className='flex flex-row gap-6'>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Thor</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                Image Here
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Thor</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                Image Here
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Thor</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                Image Here
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Thor</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                Image Here
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Thor</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                Image Here
-                            </CardContent>
-                        </Card>
+                    <div className='flex flex-row gap-6 w-full'>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <H2 className='text-md'>Strength</H2>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{thor.name}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    Image Here
+                                </CardContent>
+                            </Card>
+                        </div>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <H2 className='text-md'>Looks</H2>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{thor.name}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    Image Here
+                                </CardContent>
+                            </Card>
+                        </div>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <H2 className='text-md'>Intelligence</H2>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{thor.name}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    Image Here
+                                </CardContent>
+                            </Card>
+                        </div>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <H2 className='text-md'>Humor</H2>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{thor.name}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    Image Here
+                                </CardContent>
+                            </Card>
+                        </div>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <H2 className='text-md'>Attitude</H2>
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>{thor.name}</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    Image Here
+                                </CardContent>
+                            </Card>
+                        </div>
+                        
+                        
                     </div>
                 </div>
                
                 <div className='flex flex-col gap-5'>
                     <div>
-                        <H1>Search Superheroes </H1>
-                        <H2>Select up to 5 superheroes that sum up your personality.</H2>
+                        <H1 className='text-4xl'>Search Superheroes </H1>
+                        <H2 className='text-lg'>Select up to 5 superheroes that sum up your personality.</H2>
                     </div>
                     <Input type='text' placeholder='Search Marvel characters...' />
                     <div className='grid grid-cols-4 gap-6'>
                         <Card>
-                            <CardHeader>
+                            <CardHeader className='flex flex-row justify-between items-center'>
                                 <CardTitle>Thor</CardTitle>
                             </CardHeader>
-                            <CardContent>
+                            <CardContent className='flex flex-col gap-2'>
                                 <Image
-                                    src={thor}
+                                    src={thorpic}
                                     alt='Thor'
                                     className='w-full h-full object-cover rounded-md'
                                 />
+                                <p className='font-semibold'>Known for:</p>
+                                <ul>
+                                    <li>Being absolutely ripped</li>
+                                    <li>Smashing empty glasses</li>
+                                </ul>
+                                <Button className="mt-3 gap-2" variant={'outline'}><PlusCircle size={20} /> Add to Team</Button>
                             </CardContent>
+                            
                         </Card>
                         <Card>
                             <CardHeader>
--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Space_Grotesk } from "next/font/google"
+import { cn } from "@/lib/utils"
 
 const grotesk = Space_Grotesk({ subsets: ['latin']})
 
@@ -8,11 +9,11 @@ interface Props {
     className?: string
 }
 const H1: React.FC<Props> = ({children, className}) => {
-    return <h1 className={`${grotesk.className} ${className} text-5xl font-semibold text-slate-800`}>{children}</h1>
+    return <h1 className={cn(`${grotesk.className} text-5xl font-semibold text-slate-800`, className)}>{children}</h1>
 }
 
-const H2: React.FC<Props> = ({children}) => {
-    return <h2 className={`${grotesk.className} text-2xl text-slate-800`}>{children}</h2>
+const H2: React.FC<Props> = ({children, className}) => {
+    return <h2 className={cn(`${grotesk.className} text-2xl text-slate-800`, className)}>{children}</h2>
 }
 
 export {H1, H2};
